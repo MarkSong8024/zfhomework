a16c154113f2e22de5cbea0b7dd1629c270330f3
刘松松 <895818599@qq.com>
2019-03-26 16:09:51 +0800

    ##promise转es6

--- studentDay01/2.promise/1.promise.js
+++ studentDay01/2.promise/1.promise.js
@@ -1,87 +1,103 @@
 
-let Promise = require('./promise_shixian')
-// let p = new Promise(function (resolve, reject) {
-//     // setTimeout(function () {
-//     //     resolve('成功')
-//     // }, 1000)
-//     // reject('失败')
-//     // throw new Error('报错')
-//     resolve('123')
-// })
-// p.then().then().then(null, err => {
-//     console.log(err)
-// })
-// p.then().then().then(data => {
-//     console.log(data)
-// })
-// let p2 = p.then((data) => {
-//     return new Promise((resolve, reject) => {
+// let Promise = require('./promise_shixian')
+// // let p = new Promise(function (resolve, reject) {
+// //     // setTimeout(function () {
+// //     //     resolve('成功')
+// //     // }, 1000)
+// //     // reject('失败')
+// //     // throw new Error('报错')
+// //     resolve('123')
+// // })
+// // p.then().then().then(null, err => {
+// //     console.log(err)
+// // })
+// // p.then().then().then(data => {
+// //     console.log(data)
+// // })
+// // let p2 = p.then((data) => {
+// //     return new Promise((resolve, reject) => {
+// //         setTimeout(() => {
+// //             // resolve('xxx')
+// //             resolve(new Promise((resolve, reject) => {
+// //                 setTimeout(() => {
+// //                     resolve(10000)
+// //                 }, 1000)
+// //             }))
+// //         }, 1000)
+// //     })
+// // })
+// // p2.then(data => {
+// //     console.log(data)
+// // })
+
+// let p = new Promise((resolve, reject) => {
+//     reject(new Promise((resolve, reject) => {
 //         setTimeout(() => {
-//             // resolve('xxx')
-//             resolve(new Promise((resolve, reject) => {
-//                 setTimeout(() => {
-//                     resolve(10000)
-//                 }, 1000)
-//             }))
+//             console.log(1000)
 //         }, 1000)
-//     })
+//     }))
 // })
-// p2.then(data => {
-//     console.log(data)
-// })
-
-let p = new Promise((resolve, reject) => {
-    reject(new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log(1000)
-        }, 1000)
-    }))
-})
-p.catch(err => {
-    console.log(err)
-}).then(data => {
-    throw new Error('错误')
-}).finally(() => { // 无论如何都执行
-    console.log('100')
-}).catch(data => {
-    console.log(data)
-})
-
-p.then(r => console.log(r))
-// p.then(null, err => {
-//     console.log(err)
-// }).catch(err => {
+// p.catch(err => {
 //     console.log(err)
 // }).then(data => {
-//     console.log(data)
-// }).finally(() => {
+//     throw new Error('错误')
+// }).finally(() => { // 无论如何都执行
 //     console.log('100')
+// }).catch(data => {
+//     console.log(data)
 // })
 
+// p.then(r => console.log(r))
+// // p.then(null, err => {
+// //     console.log(err)
+// // }).catch(err => {
+// //     console.log(err)
+// // }).then(data => {
+// //     console.log(data)
+// // }).finally(() => {
+// //     console.log('100')
+// // })
 
 
-Promise.resolve(123).then(data => {
-    console.log(data, '===')
+
+// Promise.resolve(123).then(data => {
+//     console.log(data, '===')
+// })
+// Promise.reject(456).catch(data => {
+//     console.log(data, '===')
+// })
+
+// function promisify(fn) {
+//     return function () {
+//         new Promise((resolve, reject) => {
+//             fn(...arguments, function (err, data) {
+//                 if (err) reject(err)
+//                 resolve(data)
+//             })
+//         })
+//     }
+// }
+
+// function promisifyAll(obj) {
+//     for (let key in obj) {
+//         if (typeof obj[key] === 'function') {
+//             obj[key + 'Async'] = promisify(obj[key])
+//         }
+//     }
+// }
+
+// import Promise  from './promise_es6'
+let Promise = require('./promise_es6')
+let p = new Promise((resolve, reject) => {
+    // resolve('1')
+    setTimeout(() => {
+        reject(1)
+    }, 1000)
 })
-Promise.reject(456).catch(data => {
-    console.log(data, '===')
+p.then(data => {
+    console.log(data)
+}, err => {
+    console.log(err)
+}).then(data => {
+    console.log(data)
 })
-
-function promisify(fn) {
-    return function () {
-        new Promise((resolve, reject) => {
-            fn(...arguments, function (err, data) {
-                if (err) reject(err)
-                resolve(data)
-            })
-        })
-    }
-}
-
-function promisifyAll(obj) {
-    for (let key in obj) {
-        if (typeof obj[key] === 'function') {
-            obj[key + 'Async'] = promisify(obj[key])
-        }
-    }
-}
--- studentDay01/2.promise/promise_es6.js
+++ studentDay01/2.promise/promise_es6.js
@@ -1,43 +1,11 @@
-console.log('my!')
-function Promise(executor) {
-    // 给promise定义状态
-    this.status = 'pending'
-    // 成功.失败的原因
-    this.value = undefined
-    this.reason = undefined
-    let self = this
-    // 定义两个队列.存放成功.失败回调.发布订阅
-    self.onResolveCallBacks = []
-    self.onRejectedCallBacks = []
-    function reslove(value) {
-        if (value instanceof Promise) {
-            return value.then(resolve, reject)
-        }
-        if (self.status === 'pending') {
-            self.value = value
-            self.status = 'fulfilled'
-            self.onResolveCallBacks.forEach(fn => fn())
-        }
-    }
-    function reject(reason) {
-        if (self.status === 'pending') {
-            self.reason = reason
-            self.status = 'rejected'
-            self.onRejectedCallBacks.forEach(fn => fn())
-        }
-    }
-    // 执行器会立刻执行
-    try {
-        executor(reslove, reject)
-    } catch (e) { // 如果报错.调用reject方法
-        reject(e)
-    }
-}
-function resolvePromise(promise2, x, resolve, reject) {
-    if (promise2 === x) {
+
+
+
+let resolvePromise = (promise2, x, resolve, reject) => {
+    if (x === promise2) {
         return reject(new TypeError('循环引用'))
     }
-    if (typeof x !== null && (typeof x === 'object' || typeof x === 'function')) {
+    if (x !== null && (typeof x === 'object' || typeof x === 'function')) {
         let called;
         try {
             let then = x.then
@@ -45,14 +13,12 @@ function resolvePromise(promise2, x, resolve, reject) {
                 then.call(x, y => {
                     if (called) return
                     called = true
-                    resolvePromise(promise2, y, resolve, reject)
+                    resolvePromise(promise2, x, resolve, reject)
                 }, r => {
                     if (called) return
                     called = true
                     reject(r)
                 })
-            } else {
-                resolve(x)
             }
         } catch (e) {
             if (called) return
@@ -63,67 +29,135 @@ function resolvePromise(promise2, x, resolve, reject) {
         resolve(x)
     }
 }
-
-Promise.prototype.then = function (onfulfilled, onrejected) {
-    // 值的穿透
-    onfulfilled = typeof onfulfilled === 'function' ? onfulfilled : val => val
-    onrejected = typeof onrejected === 'function' ? onrejected : err => { throw err }
-    let self = this
-    let promise2 = new Promise(function (resolve, reject) {
-        if (self.status === 'fulfilled') {
-            setTimeout(() => {
-                try {
-                    let x = onfulfilled(self.value)
-                    resolvePromise(promise2, x, resolve, reject)
-                } catch (e) {
-                    reject(e)
+class Promise {
+    static resolve(value) {
+        return new Promise((resolve, reject) => {
+            resolve(value)
+        })
+    }
+    static reject(reason) {
+        return new Promise((resolve, reject) => {
+            reject(reason)
+        })
+    }
+    static all(values) {
+        return new Promise((resolve, reject) => {
+            let arr = []
+            let count = 0
+            let processData = (key, value) => {
+                arr[key] = value
+                count++
+                if (count === values.length) {
+                    resolve(arr)
                 }
-            })
-        }
-        if (self.status === 'rejected') {
-            setTimeout(() => {
-                try {
-                    let x = onrejected(self.reason)
-                    resolvePromise(promise2, x, resolve, reject)
-                } catch (e) {
-                    reject(e)
+            }
+            for (let i = 0, current; i < values.length; i++) {
+                current = values[i]
+                let then = current.then
+                if (then && typeof then === 'function') {
+                    then.call(current, y => {
+                        processData(i, y)
+                    })
+                } else {
+                    processData(i, current)
                 }
-            })
+            }
+        })
+    }
+    static race(values) {
+        return new Promise((resolve, reject) => {
+            for (let i = 0, current; i < values.length; i++) {
+                current = values[i]
+                let then = current.then
+                if (then && typeof then === 'function') {
+                    then.call(current, y => {
+                        resove(y)
+                    })
+                } else {
+                    resolve(current)
+                }
+            }
+        })
+    }
+    constructor(executer) {
+        this.status = 'pending'
+        this.value = undefined
+        this.reason = undefined
+        this.onResolveCallBacks = []
+        this.onRejectedCallBacks = []
+        let resolve = (value) => {
+            this.value = value
+            this.status = 'fulfilled'
+            this.onResolveCallBacks.forEach(fn => fn())
         }
-        if (self.status === 'pending') { // 异步.发布订阅
-            self.onResolveCallBacks.push(function () {
+        let reject = (reason) => {
+            this.reason = reason
+            this.status = 'rejected'
+            this.onRejectedCallBacks.forEach(fn => fn())
+        }
+        try {
+            executer(resolve, reject)
+        }
+        catch (e) {
+            reject(e)
+        }
+    }
+    then(onfulfilled, onrejected) {
+        let promise2 = new Promise((resolve, reject) => {
+            if (this.status === 'fulfilled') {
                 setTimeout(() => {
                     try {
-                        let x = onfulfilled(self.value)
+                        let x = onfulfilled(this.value)
                         resolvePromise(promise2, x, resolve, reject)
                     } catch (e) {
                         reject(e)
                     }
                 })
-            })
-            self.onRejectedCallBacks.push(function () {
+
+            }
+            if (this.status === 'rejected') {
                 setTimeout(() => {
                     try {
-                        let x = onrejected(self.reason)
+                        let x = onrejected(this.reason)
                         resolvePromise(promise2, x, resolve, reject)
                     } catch (e) {
                         reject(e)
                     }
                 })
-            })
-        }
-    })
-    return promise2
+            }
+            if (this.status === 'pending') {
+                this.onResolveCallBacks.push(() => {
+                    setTimeout(() => {
+                        try {
+                            let x = onfulfilled(this.value)
+                            resolvePromise(promise2, x, resolve, reject)
+                        } catch (e) {
+                            reject(e)
+                        }
+                    })
+                })
+                this.onRejectedCallBacks.push(() => {
+                    setTimeout(() => {
+                        try {
+                            let x = onrejected(this.reason)
+                            resolvePromise(promise2, x, resolve, reject)
+                        } catch (e) {
+                            reject(e)
+                        }
+                    })
+                })
+            }
+        })
+        return promise2
+    }
+    catch(errCallBack) {
+        return this.then(null, errCallBack)
+    }
+    finally(callback) {
+        return this.then(val => Promise.resolve(callback()).then(() => val),
+            reason => Promise.resolve(callback()).then(() => { throw reason }))
+    }
 }
-Promise.prototype.catch = function (errCallBack) {
-    return this.then(null, errCallBack)
-}
-
-
-
-// 测试promise是否符合A+规范
-// npm install promises-aplus-tests -g
-// 使用 promises-aplus-tests promise.js
 Promise.deferred = function () {
     let dfd = {}
     dfd.promise = new Promise((resolve, reject) => {
@@ -132,69 +166,4 @@ Promise.deferred = function () {
     })
     return dfd
 }
-Promise.resolve = function (value) {
-    return new Promise((resolve, reject) => {
-        resolve(value)
-    })
-}
-Promise.reject = function (reason) {
-    return new Promise((resolve, reject) => {
-        reject(reason)
-    })
-}
-
-Promise.prototype.finally = function (callback) { // finally 是then的别名
-    return this.then(function (value) { // 成功
-        return Promise.resolve(callback()).then(() => {
-            return value
-        })
-    }, function (reason) { // 失败
-        return Promise.resolve(callback()).then(() => {
-            throw reason
-        })
-    })
-}
-
-Promise.all = function (values) {
-    return new Promise((resolve, reject) => {
-        let arr = []
-        let count = 0
-        function processData(key, value) {
-            count++
-            arr[key] = value
-            if (count === values.length) {
-                resolve(arr)
-            }
-        }
-        for (let i = 0; i < values.length; i++) {
-            let current = values[i]
-            let then = current.then
-            if (then && typeof (then) === 'function') {
-                then.call(current, y => {
-                    processData(i, y)
-                }, reject)
-            } else {
-                processData(i, current)
-            }
-        }
-    })
-}
-
-Promise.race = function (values) {
-    return new Promise((resolve, reject) => {
-        for (let i = 0; i < values.length; i++) {
-            let current = values[i]
-            let then = current.then
-            if (then && typeof (then) === 'function') {
-                then.call(current, y => {
-                    resolve(y)
-                }, reject)
-            } else {
-                resolve(current)
-            }
-        }
-    })
-
-}
-
 module.exports = Promise
